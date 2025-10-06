3a223bc053dc1ecf907e85c07b70a0a2fe92211e
BMcMakeIt <brandon.m.mcmillan@gmail.com>
2025-10-06 12:34:16 -0500

    Delete db directory

--- db/20_indexes.sql
+++
@@ -1,7 +0,0 @@
--- Run after tables are created
-\set ON_ERROR_STOP on
-
-CREATE INDEX IF NOT EXISTS idx_inventory_part_no     ON inventory(part_no);
-CREATE INDEX IF NOT EXISTS idx_ledger_event_time     ON ledger(event_time);
-CREATE INDEX IF NOT EXISTS idx_ledger_part_no        ON ledger(part_no);
-CREATE INDEX IF NOT EXISTS idx_ledger_work_order     ON ledger(work_order_no);
